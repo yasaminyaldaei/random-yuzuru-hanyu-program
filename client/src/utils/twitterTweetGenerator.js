52a6379a01918a0f910526448aef17d6b9f9769f
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2022-04-28 14:26:55 +0430

    fix: twitter share link

--- client/src/utils/twitterTweetGenerator.js
+++ client/src/utils/twitterTweetGenerator.js
@@ -1,5 +1,7 @@
 export function TwitterTweetGenerator({ id, title }) {
   return `https://twitter.com/intent/tweet?text=${encodeURIComponent(
     `Today's Random Yuzuru Program:\n ${title}`
-  )}&url=${encodeURIComponent(`https://www.youtube.com/watch?v=${id}`)}`;
+  )}&url=${encodeURIComponent(
+    `https://www.randomyuzu.fun/deep-link?videoId=${id}`
+  )}`;
 }
