1bcfc6f40ccca46959a3536abbda8bc024be1b16
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2022-04-29 10:21:53 +0430

    fix(client): fix iframe props

--- client/src/components/SelectedVideo/index.js
+++ client/src/components/SelectedVideo/index.js
@@ -25,9 +25,9 @@ export function SelectedVideo({ videoId }) {
         height={width.current * 0.5625}
         src={`https://www.youtube.com/embed/${videoId}`}
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
         className="selected-video"
         onLoad={() => setLoading(false)}
       />
