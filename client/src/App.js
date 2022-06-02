7bc9d3f132a6edebd9fbcb00446801d74a9b72c0
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2022-06-02 16:14:53 +0430

    feat(client): add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+import { RANDOM_PROGRAM_RT, SP_3A_SBS } from "./routes";
 import RandomProgramContainer from "./components/RandomProgramContainer";
+import ShortProgram3AsSBS from "./pages/sp-3a-sbs";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="page-container">
-      <RandomProgramContainer />
+      <Routes>
+        <Route path={RANDOM_PROGRAM_RT} element={<RandomProgramContainer />} />
+        <Route path={SP_3A_SBS} element={<ShortProgram3AsSBS />} />
+      </Routes>
     </div>
   );
 }
